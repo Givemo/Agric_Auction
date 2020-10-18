04475c860a389456ee58e5a6e41f13aa7d557577
givemo <gtchiwanza@gmail.com>
2020-10-18 22:11:03 +0100

    Update 1

--- app.js
+++ app.js
@@ -1,54 +1,57 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const cors = require("cors");
-const passport = require("passport");
-const mongoose = require("mongoose");
-const config = require("./config/database");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
 
-// Connect to Database
+
+// Connect To Database (OLD CODE)
 mongoose.connect(config.database);
-
 // On Connection
-mongoose.connection.on("connected", () => {
-  console.log(`Connected to database ${config.database}`);
+mongoose.connection.on('connected', () => {
+  console.log('Connected to Database '+config.database);
 });
-
 // On Error
-mongoose.connection.on("error", (err) => {
-  console.log(`Database error: ${config.database}`);
+mongoose.connection.on('error', (err) => {
+  console.log('Database error '+err);
 });
 
 const app = express();
 
-const users = require("./routes/users");
+const users = require('./routes/users');
 
-// Port number
+// Port Number
 const port = 3000;
 
 // CORS Middleware
 app.use(cors());
 
 // Set Static Folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//Body Parser Middleware
+// Body Parser Middleware
 app.use(bodyParser.json());
 
 // Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
-app.use("/users", users);
+app.use('/users', users);
 
 // Index Route
-app.get("/", (req, res) => {
-  res.send("<h1>Invalid Endpoint</h1>");
+app.get('/', (req, res) => {
+  res.send('invaild endpoint');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/'));
 });
 
 // Start Server
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  console.log('Server started on port '+port);
 });
--- config/passport.js
+++ config/passport.js
@@ -1,27 +1,25 @@
-const JwtStrategy = require("passport-jwt").Strategy;
-const ExtractJwt = require("passport-jwt").ExtractJwt;
-const User = require("../models/user");
-const config = require("../config/database");
+  
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/user');
+const config = require('../config/database');
 
-module.exports = function (passport) {
+module.exports = function(passport) {
   let opts = {};
-  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
   opts.secretOrKey = config.secret;
-  passport.use(
-    new JwtStrategy(opts, (jwt_payload, done) => {
-      console.log(jwt_payload);
-      console.log("STRATEGY FUNCTION LOADED");
-      User.getUserById(jwt_payload._doc._id, (err, user) => {
-        if (err) {
-          return done(err, false);
-        }
-        if (user) {
-          return done(null, user);
-        } else {
-          return done(null, false);
-          // or you could create a new account
-        }
-      });
-    })
-  );
-};
+  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    console.log(jwt_payload)
+    User.getUserById(jwt_payload.data._doc._id, (err, user) => {
+      if(err) {
+        return done(err, false);
+      }
+
+      if(user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    });
+  }));
+}
